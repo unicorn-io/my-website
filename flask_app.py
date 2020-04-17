1784a3ab97b162541dd318bbb499af4a04246779
Vishesh Tayal <tayalvishesh83@gmail.com>
2020-04-17 04:36:06 +0000

    Deplyment version prebuild

---
+++ flask_app.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask           # import flask
+from flask import render_template
+app = Flask(__name__)             # create an app instance
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/contacts')
+def contacts_page():
+    return render_template("contacts.html")
+
+@app.route('/resume')
+def resume_page():
+    return render_template("resume.html")
+
+@app.route("/projects")
+def interest_page():
+    return render_template("projects.html")
+
+if __name__=="__main__":
+    app.run()
